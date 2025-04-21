0f1d252379931b5685b7e6b758f808f5ec44b63a
Maty-5 <matiasjeldres2005@gmail.com>
2025-04-21 13:30:29 -0400

    LOL

--- arreglos.py
+++ arreglos.py
@@ -1,2 +1,31 @@
 #hola, soy un comentario, LOL 
 # wololo
+
+#estatura = 1.75
+#Esdocente = True
+# Definicion de una lista
+
+#frutas = ["manzana", "pera", "uva", "mango"]
+#numeros = [1, 2, 3,]
+#print(frutas[0]) #manzana
+#print(frutas[1]) #pera
+#print(frutas[2]) #uva
+#print(frutas[3]) #mango
+#print(frutas[0:2]) #['manzana', 'pera']
+
+#estudiantes = ["Juanito", "Pedro", "Maria", "Jose"]
+#estudiantesCurso = ["Primero", "Segundo", "Tercero", "Cuarto"]
+#estudiantesEdad = [20, 21, 22, 23]
+#print(estudiantes[0], estudiantesCurso[0], estudiantesEdad[0]) 
+
+estudiantes = [
+    {'nombre' : "Juanito", 'edad' : 20, 'curso' : "Primero"},
+    {'nombre' : "Pedro", 'edad' : 21, 'curso' : "Segundo"},
+    {'nombre' : "Maria", 'edad' : 22, 'curso' : "Tercero"},
+    {'nombre' : "Jose", 'edad' : 23, 'curso' : "Cuarto"}
+    ]
+print(estudiantes[0]['nombre'], estudiantes[0]['curso'], estudiantes[0]['edad']) 
+
+
+
+
