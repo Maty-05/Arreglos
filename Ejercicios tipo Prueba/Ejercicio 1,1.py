305909cd25c5f3d948d1218b19c9e5c68226b3b6
Maty-5 <matiasjeldres2005@gmail.com>
2025-05-05 13:26:34 -0400

    lol

--- Ejercicios tipo Prueba/Ejercicio 1,1.py
+++ Ejercicios tipo Prueba/Ejercicio 1,1.py
@@ -1,13 +1,16 @@
-def normalizar_nota(nota):
-    return nota / 10 if nota > 10 else nota
+def transformar_nota(nota):
+    if nota > 7:
+        return round(nota / 10, 1)
+    return float(nota)
 
-ea1 = normalizar_nota(float(input("Ingrese la nota de la ea1: ")))
-ea2 = normalizar_nota(float(input("Ingrese la nota de la ea2: ")))
-ea3 = normalizar_nota(float(input("Ingrese la nota de la ea3: ")))
-examen = normalizar_nota(float(input("Ingrese la nota del examen: ")))
+ea1 = transformar_nota(float(input("Ingrese la nota de la EA1: ")))
+ea2 = transformar_nota(float(input("Ingrese la nota de la EA2: ")))
+ea3 = transformar_nota(float(input("Ingrese la nota de la EA3: ")))
 
-promedio_presentacion = (ea1 * 0.3 + ea2 * 0.4 + ea3 * 0.3)
-promedio_final = (promedio_presentacion * 0.6 + examen * 0.4)
+prom_presentacion = round((ea1 * 0.3 + ea2 * 0.4 + ea3 * 0.3), 1)
+print(f"El promedio de presentación final es: {prom_presentacion}")
 
-print("El promedio de presentación final es: ", round(promedio_presentacion, 1))
-print("El promedio final es: ", round(promedio_final, 1))
+examen = transformar_nota(float(input("Ingrese la nota del examen: ")))
+
+promedio_final = round((prom_presentacion * 0.6 + examen * 0.4), 1)
+print(f"El promedio final es: {promedio_final}")
