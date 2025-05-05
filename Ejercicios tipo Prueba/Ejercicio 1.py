25b081cf37acd4f68d9ee62ab91f2809ec00bfb8
Maty-5 <matiasjeldres2005@gmail.com>
2025-05-05 12:41:03 -0400

    lol

---
+++ Ejercicios tipo Prueba/Ejercicio 1.py
@@ -0,0 +1,37 @@
+#Desarrolle un programa en Python que permita calcular el promedio final de la asignatura de
+#programación de algoritmos de un estudiante. El programa debe permitir ingresar la nota de
+#la experiencia de aprendizaje 1, 2 y 3. Donde cada nota tiene una ponderación de 30%, 40%
+#y 30%. Este promedio (promedio de presentación de examen) debe mostrarse por pantalla.
+#Luego, el programa debe permitir ingresar la nota del Examen transversal y calcular el
+#promedio final. Recuerde que el promedio final se calcula como 60% el promedio de
+#presentación y 40% la nota del examen.
+#Ambos promedios mostrados por pantalla deben ir redondeados con un número decimal. Siga
+#el ejemplo a continuación:
+
+#Ejemplo 1:
+#Ingrese la nota de la EA1: 4
+#Ingrese la nota de la EA2: 5.2
+#Ingrese la nota de la EA3: 6.1
+#El promedio de presentación final es: 5.1
+#Ingrese la nota del examen: 4.0
+#El promedio final es: 4.7
+
+#Ejemplo 2:
+#Ingrese la nota de la EA1: 1
+#Ingrese la nota de la EA2: 3
+#Ingrese la nota de la EA3: 5
+#El promedio de presentación final es: 3.0
+#Ingrese la nota del examen: 5
+#El promedio final es: 3.8
+
+ea1 = float(input("Ingrese la nota de la EA1: "))
+ea2 = float(input("Ingrese la nota de la EA2: "))
+ea3 = float(input("Ingrese la nota de la EA3: "))
+examen = float(input("Ingrese la nota del examen: "))
+
+promedio_presentacion = (ea1 * 0.3 + ea2 * 0.4 + ea3 * 0.3) / 1
+promedio_final = (promedio_presentacion * 0.6 + examen * 0.4) / 1
+
+print("El promedio de presentación final es: ", round(promedio_presentacion, 1))
+print("El promedio final es: ", round(promedio_final, 1))
+
---
+++ Ejercicios tipo Prueba/Ejercicio 2.py
@@ -0,0 +1,42 @@
+#Desarrolle un programa que reciba 3 números por teclado, uno por uno, y diga: cuántos
+#números son pares y cuantos impares.
+#Además, debe indicar si la suma de todos los números es mayor a 100. Si lo es, debe indicar
+#si la suma es par o impar.
+
+#Ejemplo 1:
+#Ingrese numero: 45
+#Ingrese numero: 80
+#Ingrese numero: 21
+#Se ingresaron 1 números pares.
+#Se ingresaron 2 números impares.
+#La suma es mayor a 100.
+#La suma es par.
+
+#Ejemplo 2:
+#Ingrese numero: 20
+#Ingrese numero: 4
+#Ingrese numero: 10
+#Se ingresaron 3 números pares.
+#Se ingresaron 0 números impares.
+#La suma no es mayor a 100.
+
+pares = 0
+impares = 0
+suma = 0
+
+for i in range(3):
+    numero = int(input("Ingrese numero: "))
+    suma += numero
+    if numero % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+if suma > 100:
+    print("La suma es mayor a 100.")
+    if suma % 2 == 0:
+        print("La suma es par.")
+    else:
+        print("La suma es impar.")
+else:
+    print("La suma no es mayor a 100.")
+print("Se ingresaron", pares, "números pares.")
